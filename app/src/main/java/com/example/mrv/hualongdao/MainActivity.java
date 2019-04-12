990cbb9ef27b06e5fbce76eeb1d053ecc0d402e6
Nothing9802 <646443959@qq.com>
2019-04-12 08:10:10 +0800

    initialize my project.

---
+++ app/src/main/java/com/example/mrv/hualongdao/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mrv.hualongdao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button solder
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void change_text_color(View view) {
+
+    }
+}
